9eea88a17f01ab0cfeff3a4b83a59fac8698652a
Matheus Gitirana <matgitirana@gmail.com>
2026-01-07 08:06:01 -0300

    Added argument functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import *
 
 def get_book_text(filepath: str):
@@ -7,7 +8,13 @@ def get_book_text(filepath: str):
     return file_contents
   
 def main():
-  book_text = get_book_text('./books/frankenstein.txt')
+  if len(sys.argv) < 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
+  path_to_book = sys.argv[1]
+
+  book_text = get_book_text(f'./{path_to_book}')
 
   num_words = get_num_words(book_text)
 
@@ -16,7 +23,7 @@ def main():
   sorted_counts = sort_character_counts(character_counts)
 
   print('============ BOOKBOT ============')
-  print('Analyzing book found at books/frankenstein.txt...')
+  print(f'Analyzing book found at {path_to_book}...')
 
   print('----------- Word Count ----------')
   print(f'Found {num_words} total words')
